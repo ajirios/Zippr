75f071a9f421feaf1533cae58646f9a628660f5a
AJIRI OSAUZO JEFFREY <ajirios@outlook.com>
2025-06-07 15:25:05 -0400

    #1 Modified Gradle and AndroidManifest configurations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "uk.abord.abord"
-    compileSdk = 34
+    namespace = "app.toll.toll"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "uk.abord.abord"
-        minSdk = 24
+        applicationId = "app.toll.toll"
+        minSdk = 23
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -46,7 +48,26 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.places)
+    implementation(libs.gson)
+    implementation(libs.play.services.fitness)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.vmadalin:easypermissions-ktx:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation(platform("com.google.firebase:firebase-bom:33.13.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.firebase:firebase-auth-ktx")
+    implementation ("com.google.firebase:firebase-firestore-ktx")
+    implementation ("com.google.firebase:firebase-database-ktx")
+    implementation ("androidx.core:core-splashscreen:1.0.1")
+    implementation ("com.firebase:geofire-android:3.2.0")
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation(libs.kotlinx.coroutines.android)
+    implementation(libs.material.v1110)
+    implementation(libs.kotlin.stdlib)
 }
