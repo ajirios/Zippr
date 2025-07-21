fc4ec1b48734dd28fe0f8fad0ebe77c30fea1ae6
edwinantonie <55896428+edwinantonie@users.noreply.github.com>
2025-07-20 20:32:44 -0400

    #15 Added contact objects for chats

---
+++ app/src/main/java/app/toll/toll/model/Contact.kt
@@ -0,0 +1,8 @@
+package app.toll.toll.model
+
+data class Contact(
+    val contactName: String? = null,
+    val profilePictureUrl: String? = null,
+    val lastMessage: String? = null,
+    val lastContactedAt: String? = null
+)
